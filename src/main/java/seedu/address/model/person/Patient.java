c2c241aff123b68810632eca278220d562dc764a
Avishi-Gupta <e1355071@u.nus.edu>
2026-03-26 21:50:54 +0800

    Remove Tags

--- src/main/java/seedu/address/model/person/Patient.java
+++ src/main/java/seedu/address/model/person/Patient.java
@@ -1,17 +1,13 @@
 package seedu.address.model.person;
 
-import java.util.Set;
-
-import seedu.address.model.tag.Tag;
-
 /**
  * Represents a patient in the address book.
  * Extends {@code Person} to support the new 'addpat' command.
  */
 public class Patient extends Person {
 
-    public Patient(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        super(name, phone, email, address, tags);
+    public Patient(Name name, Phone phone, Email email, Address address) {
+        super(name, phone, email, address);
     }
 
     @Override
